0ea1cd41f5eff8e03125133272fa61b7b3a20938
Crothos <s.g.totten@gmail.com>
2022-12-23 16:48:27 -0600

    Squashed iteration bugs

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 var arrayUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var arrayLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var arraySpecial = ["~", "1", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", "|", ":", ";", "'", "<", ">", ",", ".", "?"];
+var arraySpecial = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", "|", ":", ";", "'", "<", ">", ",", ".", "?"];
 var num = Math.floor(Math.random() * 10);
 
 
@@ -17,82 +17,60 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
+// Function to generate password
 function generatePassword(){
   var passLength = prompt("Set desired password length from 8 to 128 characters.");
   var passArray = [];
-  // must return a string value that is the password.
-  // prompt user to choose num btwn 8 and 128
-    // var to save length
-    // validate user num
-  if (passLength < 8) {
-    alert("Try again, partner.");
-    return;
+// Validate Pass Length and that it's a number
+  if (passLength < 8 || passLength > 128 || isNaN(passLength) === true) {
+    alert("Try again, partner.")
+    generatePassword();
   }
-  if (passLength > 128) {
-    alert("Try again, partner.");
-    return;
-  }
-  console.log(passLength);
-  // confirm user upper, lower, num, special characters (4 confirm prompts)
-    // 4 vars to save true/false to included characters
 
   var passUpper = confirm("Would you like to include upper case characters?");
   var passLower = confirm("Would you like to include lower case characters?");
   var passNum = confirm("Would you like to include numbers?");
   var passSpecial = confirm("Would you like to include special characters?");
-  console.log(passUpper);
-  console.log(passLower);
-  console.log(passNum);
-  console.log(passSpecial);
-
-      // Validate at least one category was chosen
+// Validate at least one category was chosen
   if (passUpper === false && passLower === false && passNum === false && passSpecial === false) {
     alert("So you DON'T want a password?");
     return;
     }
 
-      // variables that include all user options of each category. Array of string for each category.
-    // var upper = ["A," "B,"]
-  // If user says yes to include that category, choose random(Math.random) element(s) from array.
-  for (var i = 0; i <= passLength;) {
-    if (passUpper === true) {
+// Each selection, if needed, will add to array and iterate once so long as it's under length value
+  for (var i = 1; i <= passLength;) {
+    if (passUpper === true && i <= passLength) {
     var randomUpper = arrayUpper[Math.floor(Math.random() * arrayUpper.length)];
     passArray.push(randomUpper);
     console.log(passArray);
     i++;
     }
   
-    if (passLower === true) {
+    if (passLower === true && i <= passLength) {
     var randomLower = arrayLower[Math.floor(Math.random() * arrayLower.length)];
     passArray.push(randomLower);
     console.log(passArray);
     i++;
     }
-
-    if (passNum === true) {
+  
+    if (passNum === true && i <= passLength) {
     var num = Math.floor(Math.random() * 10);
     passArray.push(num);
     console.log(passArray);
     i++;
     }
-
-    if (passSpecial === true) {
+   
+    if (passSpecial === true && i <= passLength) {
     var randomSpecial = arraySpecial[Math.floor(Math.random() * arraySpecial.length)];
     console.log(randomSpecial);
     passArray.push(randomSpecial);
     console.log(passArray);
     i++;
     }
-    // **This function will iterate too many times, finishing the full loop before checking if i <=passLength
-    // ** Need to apply toString() to make array string.
-    // ** Find way to randomize string itself if possible
-
-    // How to decide how many elements to choose from each array
-
-    // Join chosen elements into a string
- 
+    
+// Join chosen elements into a string with no commas
+    var passArray2 = passArray.join("");
 
   }
-  return passwordString = passArray;
+  return passwordString = passArray2;
 }
